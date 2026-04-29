229e0468bee10b5fbe7e422e3b689c67bdc62aa5
Pavan S <pavans9912@gmail.com>
2026-04-29 06:25:57 +0000

    Initial DevOps Maven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+ @Test
+ public void testMessage() {
+  assertEquals("Welcome to DevOps Lab --- Manjushree.G",
+               "Welcome to DevOps Lab --- Manjushree.G");
+ }
+}
